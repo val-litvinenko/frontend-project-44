3c6427024934598d8bf5dfb3e35537eea975cce3
valerie_l <valeriya.litvinenko.work@gmail.com>
2023-05-30 15:08:54 +0700

    Add brain-gsd game

---
+++ bin/brain-gsd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import playBrainGsd from '../src/games/brain-gsd.js';
+
+playBrainGsd();
---
+++ src/games/brain-gsd.js
@@ -0,0 +1,44 @@
+import readlineSync from 'readline-sync';
+import {
+  getRandomNumber,
+  getWrongAnswer,
+  userName,
+  SUCCESS_COUNT,
+} from '../index.js';
+
+const playBrainGsd = () => {
+  console.log('Find the greatest common divisor of given numbers.');
+
+  for (let i = 1; i <= SUCCESS_COUNT; i += 1) {
+    const randomNumFirst = getRandomNumber();
+    const randomNumSecond = getRandomNumber();
+    let bigNum = Math.max(randomNumFirst, randomNumSecond);
+    let minNum = Math.min(randomNumFirst, randomNumSecond);
+    let remainder = bigNum % minNum;
+
+    while (remainder > 0) {
+      bigNum = minNum;
+      minNum = remainder;
+      remainder = bigNum % minNum;
+    }
+
+    const gsd = minNum;
+
+    console.log(`Question: ${randomNumFirst} ${randomNumSecond}`);
+    const yourAnswer = readlineSync.question('Your answer: ');
+    const answer = parseInt(yourAnswer, 10);
+
+    if (answer === gsd) {
+      console.log('Correct!');
+      if (i === SUCCESS_COUNT) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+    } else {
+      getWrongAnswer(yourAnswer, `'${gsd}'`);
+
+      break;
+    }
+  }
+};
+
+export default playBrainGsd;
