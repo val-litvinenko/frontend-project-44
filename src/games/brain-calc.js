148f43a8e041ccd313b5f01ad465bfae5dd68d98
valerie_l <valeriya.litvinenko.work@gmail.com>
2023-05-31 17:15:38 +0700

    Refactoring

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,12 +1,13 @@
 import readlineSync from 'readline-sync';
+import getRandomChar from '../modules/get_random_char.js';
 import {
   getRandomNumber,
   getWrongAnswer,
-  getRandomChar,
   userName,
-  charArray,
   SUCCESS_COUNT,
 } from '../index.js';
+
+const charArray = ['+', '-', '*'];
 
 const calculateExpression = (a, b, operation) => {
   switch (operation) {
@@ -21,28 +22,37 @@ const calculateExpression = (a, b, operation) => {
   }
 };
 
+const generateExpression = () => {
+  const randomNumFirst = getRandomNumber(100);
+  const randomNumSecond = getRandomNumber(100);
+  const randomChar = getRandomChar(charArray);
+  const expressionResult = calculateExpression(randomNumFirst, randomNumSecond, randomChar);
+
+  const expression = `${randomNumFirst} ${randomChar} ${randomNumSecond}`;
+  const result = expressionResult;
+
+  return [expression, result];
+};
+
 const playBrainCalc = () => {
   console.log('What is the result of the expression?');
 
   for (let i = 1; i <= SUCCESS_COUNT; i += 1) {
-    const randomNumFirst = getRandomNumber(100);
-    const randomNumSecond = getRandomNumber(100);
-    const randomChar = getRandomChar(charArray);
-    const expressionResult = calculateExpression(randomNumFirst, randomNumSecond, randomChar);
+    const [expression, result] = generateExpression();
 
-    console.log(`Question: ${randomNumFirst} ${randomChar} ${randomNumSecond}`);
+    console.log(`Question: ${expression}`);
     const yourAnswer = readlineSync.question('Your answer: ');
     const answer = parseInt(yourAnswer, 10);
 
-    if (answer === expressionResult) {
+    if (answer === result) {
       console.log('Correct!');
-      if (i === SUCCESS_COUNT) {
-        console.log(`Congratulations, ${userName}!`);
-      }
     } else {
-      getWrongAnswer(yourAnswer, `'${expressionResult}'`);
-
+      getWrongAnswer(yourAnswer, result);
       break;
+    }
+
+    if (i === SUCCESS_COUNT) {
+      console.log(`Congratulations, ${userName}!`);
     }
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import greetingPlayer from './modules/greeting_player.js';
 import getRandomNumber from './modules/get_random_number.js';
-import getRandomChar from './modules/get_random_char.js';
 
 const SUCCESS_COUNT = 3;
 const MIN_PROGRESSION_LENTH = 5;
 const MIN_STEP = 1;
 
 const userName = greetingPlayer();
-
-const charArray = ['+', '-', '*'];
 
 const getWrongAnswer = (answer, condition) => {
   console.log(`'${answer}' is wrong answer ;(. Correct answer was ${condition}`);
@@ -19,9 +16,7 @@ export {
   userName,
   getRandomNumber,
   getWrongAnswer,
-  getRandomChar,
   SUCCESS_COUNT,
   MIN_PROGRESSION_LENTH,
   MIN_STEP,
-  charArray,
 };
