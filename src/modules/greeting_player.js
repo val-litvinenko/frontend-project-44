b9094b0ad9b90fbe0d077058fa8acf66fea4b2d6
valerie_l <valeriya.litvinenko.work@gmail.com>
2023-06-10 00:35:27 +0700

    Fix greeting

--- src/modules/greeting_player.js
+++ src/modules/greeting_player.js
@@ -4,7 +4,7 @@ const greetingPlayer = () => {
   console.log('Welcome to the Brain Games!');
 
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 
   return userName;
 };
