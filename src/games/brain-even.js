8d3250f32c22697b78cd2b5c96ca73a4ad9a69fe
valerie_l <valeriya.litvinenko.work@gmail.com>
2023-07-23 20:21:11 +0700

    Move even check in arrow function

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -8,13 +8,15 @@ import {
   SUCCESS_COUNT,
 } from '../index.js';
 
+const checkIsEven = (randomNum) => (randomNum % 2 === 0 ? 'yes' : 'no');
+
 const playBrainEven = () => {
   const userName = greetingPlayer();
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   for (let i = 1; i <= SUCCESS_COUNT; i += 1) {
     const randomNum = getRandomNumber(100);
-    const isEven = randomNum % 2 === 0 ? 'yes' : 'no';
+    const isEven = checkIsEven(randomNum);
 
     const yourAnswer = processQuestion(randomNum);
     const isCorrect = isCorrectAnswer(yourAnswer, isEven);
