e6a6b35e05ac87dba0a3682bcfccf2a12c9e26d3
valerie_l <valeriya.litvinenko.work@gmail.com>
2023-08-09 21:52:28 +0700

    add new modules

---
+++ src/modules/get_array_num.js
@@ -0,0 +1,26 @@
+import getRandomNumber from './get_random_number.js';
+
+const MIN_PROGRESSION_LENTH = 5;
+const MIN_STEP = 1;
+const MAX_START_RANDOM_NUM = 50;
+
+const choiseMinStep = getRandomNumber(5);
+const choiseProgressionLength = getRandomNumber(4);
+
+const getArrayNum = () => {
+  const arrayNum = [];
+  const startArray = getRandomNumber(MAX_START_RANDOM_NUM);
+  const step = choiseMinStep + MIN_STEP;
+  const arrayLenth = choiseProgressionLength + MIN_PROGRESSION_LENTH;
+  const randomIndex = getRandomNumber(arrayLenth - 1);
+  arrayNum.push(startArray);
+  for (let j = 0; j < arrayLenth; j += 1) {
+    arrayNum.push(arrayNum[j] + step);
+  }
+  const missingNumber = arrayNum[randomIndex];
+  arrayNum[randomIndex] = '..';
+
+  return [arrayNum.join(' '), missingNumber];
+};
+
+export default getArrayNum;
---
+++ src/modules/get_gsd.js
@@ -0,0 +1,22 @@
+import getRandomNumber from './get_random_number.js';
+import MAX_RANDOM_NUM from '../index.js';
+
+const getGcd = () => {
+  const randomNumFirst = getRandomNumber(MAX_RANDOM_NUM);
+  const randomNumSecond = getRandomNumber(MAX_RANDOM_NUM);
+  let bigNum = Math.max(randomNumFirst, randomNumSecond);
+  let minNum = Math.min(randomNumFirst, randomNumSecond);
+  let remainder = bigNum % minNum;
+
+  while (remainder > 0) {
+    bigNum = minNum;
+    minNum = remainder;
+    remainder = bigNum % minNum;
+  }
+
+  const gcd = minNum;
+
+  return [`${randomNumFirst} ${randomNumSecond}`, gcd];
+};
+
+export default getGcd;
